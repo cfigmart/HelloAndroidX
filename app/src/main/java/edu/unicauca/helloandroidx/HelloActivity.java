00b829b1a7a41419ba011dd6ffe93e9a0b02fd22
Cristhian Figueroa MSI <cfigmart@users.noreply.github.com>
2021-11-25 19:33:05 -0500

    este es el primer commit para la aplicación HelloAndroidX

---
+++ app/src/main/java/edu/unicauca/helloandroidx/HelloActivity.java
@@ -0,0 +1,32 @@
+package edu.unicauca.helloandroidx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Date;
+
+public class HelloActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.button3);
+        btn.setOnClickListener(this);
+        updateTime();
+    }
+
+    public void updateTime(){
+        btn.setText(new Date().toString());
+    }
+
+    @Override
+    public void onClick(View view) {
+        updateTime();
+    }
+}
